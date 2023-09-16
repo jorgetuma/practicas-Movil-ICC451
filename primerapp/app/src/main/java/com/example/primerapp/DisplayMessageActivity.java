dc987a89ea6be8cfc0bee210b90e5e99681557cd
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-09-16 12:11:03 -0400

    Iniciando con la primera app

---
+++ primerapp/app/src/main/java/com/example/primerapp/DisplayMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.primerapp;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.primerapp.databinding.ActivityDisplayMessageBinding;
+import com.example.primerapp.databinding.ActivityMainBinding;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    ActivityDisplayMessageBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        binding = ActivityDisplayMessageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("value");
+        binding.showMessage.setText(message);
+
+    }
+}
---
+++ primerapp/app/src/main/java/com/example/primerapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.primerapp;
+
+import android.content.Intent;
+import android.telephony.TelephonyCallback;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.primerapp.databinding.ActivityMainBinding;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    //Se ejecuta al presionar boton "send"
+   public void sendMesage(View view) {
+       Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+       String message = binding.txtApp.getText().toString();
+       intent.putExtra("value", message);
+       startActivity(intent);
+    }
+}
