93a72cfa61e04e51dae61759ad56f254c983c1ab
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-09-26 00:32:50 -0400

    implementando el listView

--- listartareas/app/src/main/java/com/jorgetuma/listartareas/MainActivity.java
+++ listartareas/app/src/main/java/com/jorgetuma/listartareas/MainActivity.java
@@ -1,13 +1,88 @@
 package com.jorgetuma.listartareas;
 
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.*;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.jorgetuma.listartareas.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private ArrayList<String> tareas;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        tareas = new ArrayList<>();
+
+        tareas.add("Hola");
+        tareas.add("Mundo");
+
+       adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tareas);
+        binding.appListView.setAdapter(adapter);
+        binding.appListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Eliminar tarea");
+                builder.setMessage("¿Estás seguro de que deseas eliminar esta tarea?");
+
+                //Botón "Sí"
+                builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        adapter.remove(tareas.get(position));
+                    }
+                });
+
+                //Botón "No"
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //No hacer nada.
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+
+    public void agregarTarea(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.input_tarea, null);
+
+        final EditText etNombre = dialogView.findViewById(R.id.txt_nombre);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView)
+                .setTitle("Ingrese nombre de la tarea")
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String nombre = etNombre.getText().toString();
+                        adapter.add(nombre);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //No hacer nada
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
---
+++ listartareas/app/src/main/java/com/jorgetuma/listartareas/Tarea.java
@@ -0,0 +1,13 @@
+package com.jorgetuma.listartareas;
+
+public class Tarea {
+    private final  String nombre;
+
+    public Tarea(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
