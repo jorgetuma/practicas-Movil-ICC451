b3026f0530650010333ca6a97d9109421905a604
jorgetuma <jetm0001@ce.pucmm.edu.do>
2023-10-01 13:54:52 -0400

    agregando atributo a tarea

--- listatareasRoom/app/src/main/java/com/jorgetuma/listatareasroom/Tarea.java
+++ listatareasRoom/app/src/main/java/com/jorgetuma/listatareasroom/Tarea.java
@@ -4,16 +4,22 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.UUID;
+
 @Entity
 public class Tarea {
 
-@PrimaryKey
+@PrimaryKey(autoGenerate = true)
     private int id;
 @ColumnInfo(name = "nombre")
     private String nombre;
 
+@ColumnInfo(name = "completada")
+    private boolean completada;
+
     public Tarea(String nombre) {
         this.nombre = nombre;
+        this.completada = false;
     }
 
     public String getNombre() {
@@ -23,12 +29,20 @@ public class Tarea {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
     }
 }
